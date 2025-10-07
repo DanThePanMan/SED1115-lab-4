9a29be2c954f39d6bf3d26627db6fc5867580fe5
DanThePanMan <danielchenschw@gmail.com>
2025-10-07 15:47:36 -0400

    make this work with custo messages

--- blink.py
+++ blink.py
@@ -5,11 +5,14 @@ sw5 = Pin(22, Pin.IN, Pin.PULL_DOWN)
 uart = UART(1, baudrate=9600,tx=Pin(8), rx=Pin(9))
 uart.init(bits=8, parity=None, stop=1) 
 
+
+
+
 while True:
     
     if sw5.value(): #if button is pressed
-        uart.write("hello") #write
-        print("sent") #print send
+        msg = input("enter your message: ")
+        uart.write(msg) #write
         while sw5.value(): #make it so one press sends once
             pass  
     if uart.any():
