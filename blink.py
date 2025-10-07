0a0987b0ca9920f3ba96d520ed563649f2fa36a5
DanThePanMan <danielchenschw@gmail.com>
2025-10-07 15:40:55 -0400

    Update code to send and receive

---
+++ blink.py
@@ -0,0 +1,19 @@
+from machine import Pin
+from machine import UART
+
+sw5 = Pin(22, Pin.IN, Pin.PULL_DOWN)
+uart = UART(1, baudrate=9600,tx=Pin(8), rx=Pin(9))
+uart.init(bits=8, parity=None, stop=1) 
+
+while True:
+    
+    if sw5.value(): #if button is pressed
+        uart.write("hello") #write
+        print("sent") #print send
+        while sw5.value(): #make it so one press sends once
+            pass  
+    if uart.any():
+        data = uart.read()
+        print(data)
+    
+
